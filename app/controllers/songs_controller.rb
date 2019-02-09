c61614334f50605b01c8362acc550c79a5901ca8
Melissa Real <melissa.real@hotmail.com>
2019-02-09 14:24:30 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,12 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      if Artist.find(params[:artist_id])
-        @songs = Artist.find(params[:artist_id]).songs
-      else
-        flash[:alert] = "Artist not found."
-        redirect_to artists_path
-      end
+        if Artist.find(params[:artist_id])
+          @songs = Artist.find(params[:artist_id]).songs
+        else
+          flash[:alert] = "Artist not found."
+          redirect_to artists_path
+        end
     else
       @songs = Song.all
     end
